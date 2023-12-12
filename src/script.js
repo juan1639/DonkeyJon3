5e9ce857b996420d3c0b935c6880efa627badaef
juan <juaneguiabad1639@gmail.com>
2023-12-12 03:03:39 +0100

    segundo commit

--- src/script.js
+++ src/script.js
@@ -34,12 +34,76 @@ function create() {
     platform = this.physics.add.staticGroup();
 
     platform.create(400, 568, 'ground').setScale(2).refreshBody();
-    
+
     platform.create(600, 400, 'ground');
     platform.create(50, 250, 'ground');
     platform.create(750, 220, 'ground');
+
+    player = this.physics.add.sprite(300, 150, 'dude');
+    player.setCollideWorldBounds(true);
+    player.setBounce(0.2);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [{key: 'dude', frame: 4}],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', {start: 5, end: 8}),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    player.body.setGravityY(300);
+
+    this.physics.add.collider(player, platform);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    stars = this.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: {x: 12, y: 0, stepX: 70}
+    });
+
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    this.physics.add.collider(stars, platform);
+
+    this.physics.add.overlap(player, stars, collectStar, null, true);
 }
 
 function update() {
 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+
+    } else {
+        player.setVelocityX(0);
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.setVelocityY(-600);
+    }
+}
+
+function collectStar(player, star) {
+    star.disableBody(true, true);
 }
